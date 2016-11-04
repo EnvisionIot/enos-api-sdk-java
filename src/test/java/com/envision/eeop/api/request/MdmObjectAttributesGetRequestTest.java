c93d42b8631442a216ab861c0189a97529d33088
jieyuan.shen <jieyuan.shen>
2016-11-04 10:47:43 +0000

    add test case.

--- src/test/java/com/envision/eeop/api/request/MdmObjectAttributesGetRequestTest.java
+++ src/test/java/com/envision/eeop/api/request/MdmObjectAttributesGetRequestTest.java
@@ -9,44 +9,65 @@ import org.slf4j.LoggerFactory;
 
 import com.envision.eeop.api.EnvisionClient;
 import com.envision.eeop.api.EnvisionDefaultClient;
+import com.envision.eeop.api.domain.MdmObject;
 import com.envision.eeop.api.exception.EnvisionApiException;
 import com.envision.eeop.api.response.MdmObjectAttributesGetResponse;
+import com.envision.eeop.api.response.MdmObjectStructureGetResponse;
 
 /**
  * Test eeop/mdmService/getObjectAttributes
+ * 
  * @author jieyuan.shen
  */
-public class MdmObjectAttributesGetRequestTest {
+public class MdmObjectAttributesGetRequestTest
+{
 
-	Logger logger = LoggerFactory.getLogger(MdmObjectAttributesGetRequestTest.class);
+    Logger logger = LoggerFactory.getLogger(MdmObjectAttributesGetRequestTest.class);
 
-	private static EnvisionClient client = new EnvisionDefaultClient(
-			"http://localhost:8080/eeop",
-			"a0720bb5-916c-43e2-b8e4-b129cfaf8bb6",
-			"0E133FBBFC57CA282FC2FC34C6651218");
-	
-	@Test
-	public void testWithoutAttributes() throws EnvisionApiException {
-		List<String> mdmIDList =  Arrays.asList("a77502ba622942658ca116cef5b8cf6c","a4755fbcbc4548ed82ffa33ce34c0539");
+    private static EnvisionClient client = 
+            new EnvisionDefaultClient(
+                    "http://10.21.8.124:8080/eeop", 
+                    "07bbf978-f554-423e-b73f-aa941265d54a",
+                    "78BF020CD5645E30469CD28EE44C9A6F");
+    
+    @Test
+    public void testGetObjectStructure() throws EnvisionApiException
+    {
+        MdmObjectStructureGetRequest request = new MdmObjectStructureGetRequest(
+                "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE0ODA4NDY3MTAsImNhdGVnb3J5X2xpc3QiOltdLCJ1c2VyX2lkIjoidGVzdCIsImNsaWVudF9pZCI6IjA3YmJmOTc4LWY1NTQtNDIzZS1iNzNmLWFhOTQxMjY1ZDU0YSJ9.t5_W6aL7LSjF5y2BQ9C5QCMxuY18EewIXds8DIwRhl8",
+                "102");
+        
+        MdmObjectStructureGetResponse response = client.execute(request);
 
-		MdmObjectAttributesGetRequest request = new MdmObjectAttributesGetRequest(
-				mdmIDList);
+        for (MdmObject object: response.getMdmObjects())
+        {
+            System.out.println(String.format("%s, %s, %s", object.getMdmID(), object.getAttributes().get("objectTypeID"), 
+                    object.getAttributes().get("name")));
+        }
+    }
 
-		MdmObjectAttributesGetResponse response = client.execute(request);
-		System.out.println(response.getBody());
-		System.out.println(response.toString());		
-	}
+    @Test
+    public void testWithoutAttributes() throws EnvisionApiException
+    {
+        List<String> mdmIDList = Arrays.asList("a77502ba622942658ca116cef5b8cf6c", "a4755fbcbc4548ed82ffa33ce34c0539");
 
-	@Test
-	public void testWithAttributes() throws EnvisionApiException {
-		List<String> mdmIDList =  Arrays.asList("a77502ba622942658ca116cef5b8cf6c","a4755fbcbc4548ed82ffa33ce34c0539");
-		List<String> attributeList = Arrays.asList("name", "branches");
-				
-		MdmObjectAttributesGetRequest request = new MdmObjectAttributesGetRequest(
-				mdmIDList, attributeList);
+        MdmObjectAttributesGetRequest request = new MdmObjectAttributesGetRequest(mdmIDList);
 
-		MdmObjectAttributesGetResponse response = client.execute(request);
-		System.out.println(response.getBody());
-		System.out.println(response.toString());		
-	}
+        MdmObjectAttributesGetResponse response = client.execute(request);
+        System.out.println(response.getBody());
+        System.out.println(response.toString());
+    }
+
+    @Test
+    public void testWithAttributes() throws EnvisionApiException
+    {
+        List<String> mdmIDList = Arrays.asList("a77502ba622942658ca116cef5b8cf6c", "a4755fbcbc4548ed82ffa33ce34c0539");
+        List<String> attributeList = Arrays.asList("name", "branches");
+
+        MdmObjectAttributesGetRequest request = new MdmObjectAttributesGetRequest(mdmIDList, attributeList);
+
+        MdmObjectAttributesGetResponse response = client.execute(request);
+        System.out.println(response.getBody());
+        System.out.println(response.toString());
+    }
 }
