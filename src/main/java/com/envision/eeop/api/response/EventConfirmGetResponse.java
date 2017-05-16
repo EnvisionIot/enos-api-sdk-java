ce157c595454fe8c42641a62bf5bb85b2a774ccc
liming.yang <liming.yang>
2017-05-16 08:33:29 +0000

    update

--- src/main/java/com/envision/eeop/api/response/EventConfirmGetResponse.java
+++ src/main/java/com/envision/eeop/api/response/EventConfirmGetResponse.java
@@ -4,4 +4,9 @@ import com.envision.eeop.api.EnvisionResponse;
 
 public class EventConfirmGetResponse extends EnvisionResponse{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4770910600781815692L;
+
 }
