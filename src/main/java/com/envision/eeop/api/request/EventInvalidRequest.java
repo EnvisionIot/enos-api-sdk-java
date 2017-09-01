2a290b584c089ce399e6a522dac4b3c2422186a6
liming.yang <liming.yang>
2017-09-01 03:16:36 +0000

    update

--- src/main/java/com/envision/eeop/api/request/EventInvalidRequest.java
+++ src/main/java/com/envision/eeop/api/request/EventInvalidRequest.java
@@ -5,27 +5,25 @@ import java.util.Map;
 
 import com.envision.eeop.api.EnvisionRequest;
 import com.envision.eeop.api.exception.EnvisionRuleException;
-import com.envision.eeop.api.response.EventConfirmGetResponse;
 import com.envision.eeop.api.response.EventInvalidResponse;
 import com.envision.eeop.api.util.RuleCheckUtils;
-import com.envision.eos.event.api.bo.LanguageType;
 
 public class EventInvalidRequest implements EnvisionRequest<EventInvalidResponse> {
 
 	private static final String API_METHOD = "/eventService/invalid";
 
-	private String globalId;
+	private String globalIds;
 	private boolean includingHistory;
 
-	public EventInvalidRequest(String globalId) {
-		this.globalId = globalId;
+	public EventInvalidRequest(String globalIds) {
+		this.globalIds = globalIds;
 		this.includingHistory = false;
 	}
 
-//	public EventInvalidRequest(String globalId, boolean includingHistory) {
-//		this.globalId = globalId;
-//		this.includingHistory = includingHistory;
-//	}
+	// public EventInvalidRequest(String globalId, boolean includingHistory) {
+	// this.globalId = globalId;
+	// this.includingHistory = includingHistory;
+	// }
 
 	@Override
 	public String getApiMethodName() {
@@ -35,7 +33,7 @@ public class EventInvalidRequest implements EnvisionRequest<EventInvalidResponse
 	@Override
 	public Map<String, String> getTextParams() {
 		Map<String, String> params = new HashMap<String, String>();
-		params.put("globalId", globalId);
+		params.put("globalIds", globalIds);
 		params.put("includingHistory", "" + includingHistory);
 
 		return params;
@@ -48,7 +46,7 @@ public class EventInvalidRequest implements EnvisionRequest<EventInvalidResponse
 
 	@Override
 	public void check() throws EnvisionRuleException {
-		RuleCheckUtils.checkNotEmpty(globalId, "globalId");
+		RuleCheckUtils.checkNotEmpty(globalIds, "globalIds");
 	}
 
 }
