90bd859e50a1a18ba2aa9bba14f65b13fd40f133
liming.yang <liming.yang>
2017-11-29 06:16:24 +0000

    update

--- src/main/java/com/envision/eeop/api/util/EventJsonParser.java
+++ src/main/java/com/envision/eeop/api/util/EventJsonParser.java
@@ -2,7 +2,10 @@ package com.envision.eeop.api.util;
 
 import java.lang.reflect.Type;
 
+import com.envision.eos.event.api.expression.DateExpr;
+import com.envision.eos.event.api.expression.Expression;
 import com.envision.eos.event.api.expression.Filter;
+import com.envision.eos.event.api.expression.HourExpr;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonDeserializationContext;
@@ -16,6 +19,8 @@ import com.google.gson.JsonSerializer;
 public class EventJsonParser {
 
 	private static final Gson gson = new GsonBuilder().registerTypeAdapter(Filter.class, new FilterDeserializer())
+			.registerTypeAdapter(DateExpr.class, new ExpressionDeserializer())
+			.registerTypeAdapter(HourExpr.class, new ExpressionDeserializer())
 			.create();
 
 	private static final String FILTER_PREFIX = "com.envision.eos.event.api.expression.";
@@ -62,6 +67,35 @@ public class EventJsonParser {
 
 		@Override
 		public JsonElement serialize(Filter src, Type typeOfSrc, JsonSerializationContext context) {
+			JsonElement json = context.serialize(src, src.getClass());
+			return json;
+		}
+
+	}
+	
+	public static class ExpressionDeserializer implements JsonSerializer<Expression>, JsonDeserializer<Expression> {
+
+		@Override
+		public Filter deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+				throws JsonParseException {
+			try {
+				String type = ((JsonObject) json).get("type").getAsString();
+				
+				type=FILTER_PREFIX+type;
+
+				Class<?> klass = Class.forName(type);
+
+				return context.deserialize(((JsonObject) json), klass);
+
+			} catch (ClassNotFoundException e) {
+
+			}
+
+			return null;
+		}
+
+		@Override
+		public JsonElement serialize(Expression src, Type typeOfSrc, JsonSerializationContext context) {
 			JsonElement json = context.serialize(src, src.getClass());
 			return json;
 		}
