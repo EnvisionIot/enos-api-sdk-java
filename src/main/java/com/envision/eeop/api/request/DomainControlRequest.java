c2768cdac67c621d680de2d97254b2ef186b2363
jieyuan.shen <jieyuan.shen>
2018-01-09 10:24:52 +0000

    support control multiple points simutaneously

--- src/main/java/com/envision/eeop/api/request/DomainControlRequest.java
+++ src/main/java/com/envision/eeop/api/request/DomainControlRequest.java
@@ -6,70 +6,96 @@ import com.envision.eeop.api.exception.EnvisionRuleException;
 import com.envision.eeop.api.util.EnvisionHashMap;
 import com.envision.eeop.api.util.RuleCheckUtils;
 import com.envision.eeop.api.util.StringUtils;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 /**
  * Created by changyi.yuan on 2016/6/19.
  */
-public class DomainControlRequest implements EnvisionRequest<EnvisionResponse> {
+public class DomainControlRequest implements EnvisionRequest<EnvisionResponse>
+{
     private static final String API_METHOD = "/controlService/control";
 
-    private String mdmid; //mandatory
+    private String mdmid;                   // mandatory
 
-    private String point; //mandatory
-
-    private String parameter; // optional
+    private Map<String,String> pointsJson;  // mandatory
 
     /**
-	 * @param mdmID
-	 * @param point
-	 * @param parameter
-	 */
-	public DomainControlRequest(String mdmid, String point) {
-		super();
-		this.mdmid = mdmid;
-		this.point = point;
-	}
+     * construct a control request
+     * @param pointsJson one or multiple control points and parameters
+     */
+    public DomainControlRequest(String mdmid, Map<String,String> pointsJson)
+    {
+        this.mdmid = mdmid;
+        this.pointsJson = pointsJson;
+    }
 
     /**
-	 * @param mdmID
-	 * @param point
-	 * @param parameter
-	 */
-	public DomainControlRequest(String mdmid, String point, String parameter) {
-		this(mdmid, point);
-		this.parameter = parameter;
-	}
-	
+     * @param mdmID
+     * @param point
+     * @param parameter
+     * @deprecated use {@link #DomainControlRequest(String, Map)} instead
+     */
+    public DomainControlRequest(String mdmid, String point)
+    {
+        this.mdmid = mdmid;
+        this.pointsJson = new HashMap<>();
+        this.pointsJson.put(point, "");
+    }
+
+    /**
+     * @param mdmID
+     * @param point
+     * @param parameter
+     * @deprecated use {@link #DomainControlRequest(String, Map)} instead
+     */
+    public DomainControlRequest(String mdmid, String point, String parameter)
+    {
+        this.mdmid = mdmid;
+        this.pointsJson = new HashMap<>();
+        this.pointsJson.put(point, parameter);
+    }
 
     @Override
-    public String getApiMethodName() {
+    public String getApiMethodName()
+    {
         return API_METHOD;
     }
 
     @Override
-	public Map<String, String> getTextParams() {
+    public Map<String, String> getTextParams()
+    {
         EnvisionHashMap txtParams = new EnvisionHashMap();
 
         txtParams.put("mdmid", mdmid);
-        txtParams.put("point", point);
-        
-        if(!StringUtils.isEmpty(parameter)){
-        	 txtParams.put("parameter", parameter);
+        txtParams.put("points", 
+                new Gson().toJson(pointsJson, new TypeToken<Map<String,String>>(){}.getType()));
+
+        // TODO: to be removed in future release
+        Entry<String,String> firstPoint = pointsJson.entrySet().iterator().next();
+        txtParams.put("point", firstPoint.getKey());
+        if (!StringUtils.isEmpty(firstPoint.getValue()))
+        {
+            txtParams.put("parameter", firstPoint.getValue());
         }
 
         return txtParams;
     }
 
     @Override
-    public Class<EnvisionResponse> getResponseClass() {
+    public Class<EnvisionResponse> getResponseClass()
+    {
         return EnvisionResponse.class;
     }
 
     @Override
-    public void check() throws EnvisionRuleException {
+    public void check() throws EnvisionRuleException
+    {
         RuleCheckUtils.checkNotEmpty(mdmid, "mdmid");
-        RuleCheckUtils.checkNotEmpty(point, "point");
+        RuleCheckUtils.checkNotEmpty(pointsJson, "points");
     }
 }
