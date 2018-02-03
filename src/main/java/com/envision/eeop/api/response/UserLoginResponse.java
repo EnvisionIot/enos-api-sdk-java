2e1ef7f0f112ac407447f7f2e7bc73274e4f7fee
jieyuan.shen <jieyuan.shen>
2018-02-03 08:21:48 +0000

    add toString for UserLoginResponse

--- src/main/java/com/envision/eeop/api/response/UserLoginResponse.java
+++ src/main/java/com/envision/eeop/api/response/UserLoginResponse.java
@@ -8,7 +8,8 @@ import java.util.Map;
 /**
  * Created by zhiqi.yang on 2017/5/14.
  */
-public class UserLoginResponse extends EnvisionResponse {
+public class UserLoginResponse extends EnvisionResponse
+{
     private static final long serialVersionUID = -6836336446303410755L;
     private String userId;
     private String userName;
@@ -18,59 +19,73 @@ public class UserLoginResponse extends EnvisionResponse {
     private Long refreshTokenExpire;
     private Map<String, String> attributeMap = new HashMap<>();
 
-    public String getUserId() {
+    public String getUserId()
+    {
         return userId;
     }
 
-    public void setUserId(String userId) {
+    public void setUserId(String userId)
+    {
         this.userId = userId;
     }
 
-    public String getUserName() {
+    public String getUserName()
+    {
         return userName;
     }
 
-    public void setUserName(String userName) {
+    public void setUserName(String userName)
+    {
         this.userName = userName;
     }
 
-    public String getAccessToken() {
+    public String getAccessToken()
+    {
         return accessToken;
     }
 
-    public void setAccessToken(String accessToken) {
+    public void setAccessToken(String accessToken)
+    {
         this.accessToken = accessToken;
     }
 
-    public Long getAccessTokenExpire() {
+    public Long getAccessTokenExpire()
+    {
         return accessTokenExpire;
     }
 
-    public void setAccessTokenExpire(Long accessTokenExpire) {
+    public void setAccessTokenExpire(Long accessTokenExpire)
+    {
         this.accessTokenExpire = accessTokenExpire;
     }
 
-    public String getRefreshToken() {
+    public String getRefreshToken()
+    {
         return refreshToken;
     }
 
-    public void setRefreshToken(String refreshToken) {
+    public void setRefreshToken(String refreshToken)
+    {
         this.refreshToken = refreshToken;
     }
 
-    public Long getRefreshTokenExpire() {
+    public Long getRefreshTokenExpire()
+    {
         return refreshTokenExpire;
     }
 
-    public void setRefreshTokenExpire(Long refreshTokenExpire) {
+    public void setRefreshTokenExpire(Long refreshTokenExpire)
+    {
         this.refreshTokenExpire = refreshTokenExpire;
     }
 
-    public Map<String, String> getAttributeMap() {
+    public Map<String, String> getAttributeMap()
+    {
         return attributeMap;
     }
 
-    public void setAttributeMap(Map<String, String> attributeMap) {
+    public void setAttributeMap(Map<String, String> attributeMap)
+    {
         this.attributeMap = attributeMap;
     }
 
@@ -79,6 +94,7 @@ public class UserLoginResponse extends EnvisionResponse {
     {
         return "UserLoginResponse [userId=" + userId + ", userName=" + userName + ", accessToken=" + accessToken
                 + ", accessTokenExpire=" + accessTokenExpire + ", refreshToken=" + refreshToken
-                + ", refreshTokenExpire=" + refreshTokenExpire + ", attributeMap=" + attributeMap + "]";
+                + ", refreshTokenExpire=" + refreshTokenExpire + ", attributeMap=" + attributeMap + ", status=" + status
+                + ", msg=" + msg + ", submsg=" + submsg + "]";
     }
 }
